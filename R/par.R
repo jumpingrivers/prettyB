b35ad1013aac9359d453c67e1da101a01c01b5cd
Colin Gillespie <csgillespie@gmail.com>
2016-10-18 21:45:20 +0100

    Rethink par caching

--- R/par.R
+++ R/par.R
@@ -1,28 +1,26 @@
-#' @importFrom graphics par
-#' @export
-par = function(..., no.readonly = FALSE) {
-  new_args = list(...)
-  ## Keep track of changes in par
-  cache$par[names(new_args)] = new_args
-  graphics::par(..., no.readonly = no.readonly)
+set_par = function(new_par) {
+  new_par_names = names(new_par)
+  cur_par = par()
+  cached_par = cache$par
+
+  changes = vapply(new_par_names,
+                   function(i) identical(cur_par[[i]], cached_par[[i]]),
+                   logical(1))
+  graphics::par(new_par[changes])
 }
 
-
 set_par_expand = function() {
-
-  par_exp = list(mar = c(3, 3, 2, 1), mgp = c(2, 0.4, 0), tck = -.01,
-    cex.axis = 0.9, las = 1)
-  cur_par = cache$par
-  par_exp[names(cur_par)] = cur_par
-  graphics::par(par_exp)
+  par_exp = list(mar = c(3, 3, 2, 1), mgp = c(2, 0.4, 0),
+                 tck = -.01,
+                 cex.axis = 0.9, las = 1)
+  set_par(par_exp)
 }
 
 
 set_par_minimal = function() {
-
   par_min = list(mar = c(3, 3, 2, 1), mgp = c(2, 0.4, 0), tck = -.01,
-                 cex.axis = 0.9, las = 1, xaxs='i',yaxs='i', col.lab="grey50")
-  cur_par = cache$par
-  par_min[names(cur_par)] = cur_par
-  graphics::par(par_min)
+                 cex.axis = 0.9, las = 1,
+                 xaxs='i', yaxs='i',
+                 col.lab="grey50")
+  set_par(par_min)
 }
