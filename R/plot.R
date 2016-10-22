6265d8f2801020376b4e0c17308e5bd3d8032093
Colin Gillespie <csgillespie@gmail.com>
2016-10-22 21:29:22 +0100

    deparse xlab/ylab

--- R/plot.R
+++ R/plot.R
@@ -1,11 +1,18 @@
 #' @importFrom graphics plot.default abline axTicks axis grid par title
-#' @export
+#' @export "plot.default"
 plot.default = function(x, y = NULL, ...) {
+  xlab = deparse(substitute(x))
+  if(!is.null(y)) {
+    ylab = deparse(substitute(y))
+  } else {
+    ylab = xlab
+    xlab = NULL
+  }
   theme = cache$theme
   if(theme == "expand") {
-    plot_expand(x, y, ...)
+    plot_expand(x, y, xlab=xlab, ylab=ylab, ...)
   } else if(theme == "minimal") {
-    plot_minimal(x, y, ...)
+    plot_minimal(x, y, xlab=xlab, ylab=ylab, ...)
   }
 }
 
--- R/plot_expand.R
+++ R/plot_expand.R
@@ -1,12 +1,13 @@
 
-plot_expand = function(x, y = NULL, ...) {
+plot_expand = function(x, y = NULL, xlab, ylab, ...) {
 
   ## Set up par
   op = set_par_expand()
   on.exit(graphics::par(op))
 
   ## Now args
-  new_args = list(pch = 21, bg = 1)
+
+  new_args = list(pch = 21, bg = 1, xlab = xlab, ylab=ylab)
   old_args = list(...)
   new_args[names(old_args)] = old_args
   if (is.null(y))
