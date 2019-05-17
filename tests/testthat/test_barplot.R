ef15d804975ed9e30506517c97eb864d6c658378
Colin Gillespie <csgillespie@gmail.com>
2019-05-17 08:31:21 +0100

    lintr error

--- tests/testthat/test_barplot.R
+++ tests/testthat/test_barplot.R
@@ -1,8 +1,10 @@
 test_that("Testing barplot", {
   context("Barplot")
   testthat::skip_on_travis()
-  vdiffr::expect_doppelganger("barplot basic", barplot_p(VADeaths))
-  vdiffr::expect_doppelganger("barplot horiz", barplot_p(VADeaths, horiz = TRUE))
+  vdiffr::expect_doppelganger("barplot basic",
+                              barplot_p(VADeaths))
+  vdiffr::expect_doppelganger("barplot horiz",
+                              barplot_p(VADeaths, horiz = TRUE))
   vdiffr::expect_doppelganger("barplot labels",
                               barplot_p(VADeaths, xlab = "X", ylab = "Y",
                                       main = "Main", sub = "Sub"))
